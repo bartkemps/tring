b18a29da82c65ef374cbf6dd7fc2055f1b8ccc83
Bart Kemps ☺ <b.kemps@rdc.nl>
2025-06-02 11:44:21 +0200

    Refactor Negative operation tests for clarity and consistency

--- Tring.Tests/Operators/OperatorTests.cs
+++ Tring.Tests/Operators/OperatorTests.cs
@@ -6,26 +6,28 @@ using Tring.Operators;
 
 public class OperatorTests
 {
-    
+
     [Theory]
-    [InlineData(-1, 1)]
-    public void Negative_ExecutesCorrectly(sbyte value,sbyte expected)
+    [InlineData(-1, -1)]
+    public void Negative_ExecutesCorrectly(sbyte value, sbyte expectedValue)
+
     {
         var array = new TritArray27();
         var trit = (Trit)value;
-        
+        var expected = (Trit)expectedValue;
+
         var actual = trit | Operator.Negative;
-        actual.Should().Be((Trit)expected);
-        
+        actual.Should().Be(expected);
+
         actual = trit | UnaryOperation.Negative;
-        actual.Should().Be((Trit)expected);
-        
+        actual.Should().Be(expected);
+
         array[1] = trit;
         var result = array | Operator.Negative;
-        result[1].Should().Be((Trit)expected);
+        result[1].Should().Be(expected);
 
         array[1] = trit;
         result = array | UnaryOperation.Negative;
-        result[1].Should().Be((Trit)expected);
+        result[1].Should().Be(expected);
     }
 }
