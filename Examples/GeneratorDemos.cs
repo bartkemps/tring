5ec398eebb43638b51cd30fba0385bc068f9f309
Bart Kemps ☺ <b.kemps@rdc.nl>
2025-06-26 14:23:14 +0200

    Add Conversion class for ternary string to long conversion and update generator logic

--- Examples/GeneratorDemos.cs
+++ Examples/GeneratorDemos.cs
@@ -1,18 +1,17 @@
 ﻿[assembly: Ternary3.GenerateTernaryConstants]
+
 namespace Examples;
 
 using Ternary3;
 
 public partial class GeneratorDemos
 {
-    private string terT1111_11 = "hi";
-    
     public static void Run()
     {
         Console.WriteLine($"The ternary value of T01 is {terT01}");
         var sum = terT01 + ter111_111;
         Console.WriteLine($"{nameof(terT01)} + {nameof(ter111_111)} = {sum} ({sum:ter})");
         Int27T int27Value = terTTT000111;
-        TritArray27 tritArrayValue = terT01T01T01;
+        TritArray27 tritArrayValue = ter111111111_000000000_TTTTTTTTT;
     }
 }
---
+++ Ternary3.Generators/Conversion.cs
@@ -0,0 +1,20 @@
+﻿namespace Ternary3.Generators;
+
+using System.Linq;
+
+public class Conversion
+{
+    public static long ToLong(string digits)
+    {
+        long value = 0;
+        long scale = 1;
+        foreach (var digit in digits.Reverse())
+        {
+            var i = "T01".IndexOf(digit);
+            if (i==-1) continue;
+            value += i * scale;
+            scale *= 3;
+        }
+        return value;
+    }
+}
