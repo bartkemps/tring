06dafe5b2eb2d71939a97877f97ccdf59ddb3997
Bart Kemps ☺ <b.kemps@rdc.nl>
2025-07-07 12:21:00 +0200

    Replace `BinaryTritEncoderTests` with `Int3ToByteStream` and `ByteToInt3TStream`
    - Removed `BinaryTritEncoderTests` unit test class.
    - Introduced `Int3ToByteStream` for converting Int3T streams to byte streams and vice versa with `ByteToInt3TStream`.
    - Refactored `BinaryTritEncoder` to support magic number handling, flushing, and section headers.

--- Ternary3/IO/BinaryTritEncoder.cs
+++ Ternary3/IO/BinaryTritEncoder.cs
@@ -1,55 +1,72 @@
 ﻿namespace Ternary3.IO;
 
-internal class BinaryTritEncoder
+/// <summary>
+/// Note: the consumer of this class needs to ensure it is either used for reading or for writing, not both.
+/// </summary>
+internal class BinaryTritEncoder(bool mustWriteMagicNumber = true)
 {
+    private bool mustWriteMagicNumber = mustWriteMagicNumber;
+    private bool mustWriteSectionHeader = mustWriteMagicNumber;
+    private int offset;
+    private int workingValue;
+
     public IEnumerable<byte> Encode(IEnumerable<Int3T> source)
     {
-        yield return 245; // Magic number to indicate this is a binary trit encoding.
-        yield return 244; // Magic number for the encoding format.
-        var offset = 0;
-        int outputByte = 0;
         foreach (var int3T in source)
         {
+            if (mustWriteMagicNumber)
+            {
+                yield return 245; // Magic number to indicate this is a binary trit encoding.
+                mustWriteMagicNumber = false;
+            }
+
+            if (mustWriteSectionHeader)
+            {
+                yield return 244; // Magic number for the encoding format.
+                mustWriteMagicNumber = false;
+            }
+
             var val = (int)int3T + 13;
             switch (offset)
             {
                 case 0:
-                    outputByte = val;
+                    workingValue = val;
                     offset = 3;
                     break;
                 case 1:
-                    outputByte += 3 * val;
+                    workingValue += 3 * val;
                     offset = 4;
                     break;
                 case 2:
-                    outputByte += 9 * val;
-                    yield return (byte)outputByte;
+                    workingValue += 9 * val;
+                    yield return (byte)workingValue;
                     offset = 0;
                     break;
                 case 3:
-                    outputByte += 27 * (val % 9);
-                    yield return (byte)outputByte;
-                    outputByte = val / 9;
+                    workingValue += 27 * (val % 9);
+                    yield return (byte)workingValue;
+                    workingValue = val / 9;
                     offset = 1;
                     break;
                 default:
-                    outputByte += 81 * (val % 3);
-                    yield return (byte)outputByte;
-                    outputByte = val / 3;
+                    workingValue += 81 * (val % 3);
+                    yield return (byte)workingValue;
+                    workingValue = val / 3;
                     offset = 2;
                     break;
             }
         }
+    }
 
+    public IEnumerable<byte> Flush()
+    {
         if (offset == 0) yield break;
-        // return the magically encoded rest
-        yield return offset < 3 ? (byte)(255 - outputByte) : (byte)outputByte;
+        yield return offset < 3 ? (byte)(255 - workingValue) : (byte)workingValue;
+        mustWriteSectionHeader = true;
     }
 
     public IEnumerable<Int3T> Decode(IEnumerable<byte> source)
     {
-        var offset = 0;
-        int outputInt3T = 0;
         foreach (var b in source)
         {
             switch (b)
@@ -59,24 +76,24 @@ internal class BinaryTritEncoder
                     switch (offset)
                     {
                         case 0:
-                            outputInt3T = b % 27;
-                            yield return (Int3T)(outputInt3T - 13);
-                            outputInt3T = b / 27;
+                            workingValue = b % 27;
+                            yield return (Int3T)(workingValue - 13);
+                            workingValue = b / 27;
                             offset = 2;
                             continue;
                         case 1:
-                            outputInt3T += 3 * (b % 9);
-                            yield return (Int3T)(outputInt3T - 13);
-                            outputInt3T = b / 9;
-                            yield return (Int3T)(outputInt3T - 13);
+                            workingValue += 3 * (b % 9);
+                            yield return (Int3T)(workingValue - 13);
+                            workingValue = b / 9;
+                            yield return (Int3T)(workingValue - 13);
                             offset = 0;
                             continue;
                         default: // offset = 2;
-                            outputInt3T += 9 * (b % 3);
-                            yield return (Int3T)(outputInt3T - 13);
-                            outputInt3T = (b / 3) % 27;
-                            yield return (Int3T)(outputInt3T - 13);
-                            outputInt3T = b / 81;
+                            workingValue += 9 * (b % 3);
+                            yield return (Int3T)(workingValue - 13);
+                            workingValue = (b / 3) % 27;
+                            yield return (Int3T)(workingValue - 13);
+                            workingValue = b / 81;
                             offset = 1;
                             continue;
                     }
@@ -85,12 +102,12 @@ internal class BinaryTritEncoder
                     switch (offset)
                     {
                         case 1:
-                            outputInt3T += (255 - b) % 9;
-                            yield return (Int3T)(outputInt3T - 13);
+                            workingValue += 3 * ((255 - b) % 9);
+                            yield return (Int3T)(workingValue - 13);
                             break;
                         case 2:
-                            outputInt3T += (255 - b) % 3;
-                            yield return (Int3T)(outputInt3T - 13);
+                            workingValue += 9 * ((255 - b) % 3);
+                            yield return (Int3T)(workingValue - 13);
                             break;
                         // case 0 would be strange because there's no need ever to write like this,
                         // even if you need intermittent flushes
---
+++ Ternary3/IO/ByteToInt3TStream.cs
@@ -0,0 +1,61 @@
+﻿namespace Ternary3.IO;
+
+using System;
+using System.IO;
+
+/// <summary>
+/// A stream that converts a byte stream to an Int3T stream.
+/// </summary>
+public sealed class ByteToInt3TStream(Stream source, bool mustWriteMagicNumber = true) : Int3TStream
+{
+    private bool canRead = source.CanRead;
+    private bool canWrite = source.CanWrite;
+    private BinaryTritEncoder encoder = new BinaryTritEncoder(mustWriteMagicNumber);
+    public override async Task<int> ReadAsync(Int3T[] buffer, int offset, int count, CancellationToken cancellationToken = default)
+    {
+        if (!CanRead) throw new NotSupportedException("The stream does not support reading.");
+        canWrite = false;
+        throw new NotImplementedException();
+    }
+
+    public override async Task WriteAsync(Int3T[] buffer, int offset, int count, CancellationToken cancellationToken = default)
+    {
+        if (!CanWrite) throw new NotSupportedException("The stream does not support writing.");
+        canRead = false;
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Flushes the stream, writing any buffered data to the underlying stream.
+    /// Warning: flushing will restart part of the encoding, making size optimization impossible.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token</param>
+    /// <exception cref="NotSupportedException"></exception>
+    public override async Task FlushAsync(CancellationToken cancellationToken = default)
+    {
+        if (!CanWrite) throw new NotSupportedException("The stream does not support writing.");
+        canRead = false;
+        var bytes = encoder.Flush().ToArray();
+        await source.WriteAsync(bytes, cancellationToken);
+        await source.FlushAsync(cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public override Task<long> SeekAsync(long offset, SeekOrigin origin, CancellationToken cancellationToken = default) => throw new NotSupportedException();
+    /// <inheritdoc />
+    public override Task SetLengthAsync(long value, CancellationToken cancellationToken = default) => throw new NotSupportedException();
+    /// <inheritdoc />
+    public override bool CanRead => source.CanRead;
+    /// <inheritdoc />
+    public override bool CanWrite => source.CanWrite;
+    /// <inheritdoc />
+    public override bool CanSeek => false;
+    /// <inheritdoc />
+    public override long Length => throw new NotSupportedException();
+    /// <inheritdoc />
+    public override long Position
+    {
+        get => throw new NotSupportedException("The stream does not support seeking.");
+        set => throw new NotSupportedException("The stream does not support seeking.");
+    }
+}
