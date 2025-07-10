a3403f21f9d19db832c2181a7d2ca04d4ba90ea0
Bart Kemps ☺ <b.kemps@rdc.nl>
2025-07-10 02:31:04 +0200

    failing test for shift right

---
+++ Ternary3.Tests/Numbers/TritArray27Int27TShiftConsistencyTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Ternary3.Tests.Numbers;
+
+public class TritArray27Int27TShiftConsistencyTests
+{
+    [Theory]
+    [InlineData(3812798742493L, 0)] // Max value, no shift
+    [InlineData(3812798742493L, 1)] // Max value, shift by 1
+    [InlineData(3812798742493L, 13)] // Max value, shift by 13
+    [InlineData(3812798742493L, 26)] // Max value, shift by 26
+    [InlineData(3812798742493L, 27)] // Max value, shift by 27 (should be zero)
+    [InlineData(-3812798742493L, 5)] // Min value, shift by 5
+    [InlineData(1234567890L, 7)] // Arbitrary big number, shift by 7
+    [InlineData(-1234567890L, 7)] // Negative big number, shift by 7
+    public void RightShift_YieldsSameValue_ForTritArray27AndInt27T(long value, int shift)
+    {
+        Int27T intValue = value;
+        TritArray27 tritArray = intValue;
+
+        var shiftedArray = tritArray >> shift;
+        var shiftedInt = intValue >> shift;
+        TritArray27 shiftedArrayFromInt = shiftedInt;
+
+        for (var i = 0; i < 27; i++)
+        {
+            shiftedArray[i].Should().Be(shiftedArrayFromInt[i], $"because right shift of {value} by {shift} should yield the same trits for TritArray27 and Int27T");
+        }
+    }
+}
+
